a3c346e5f6caeea451c6d8a9ea35919fe66bced3
sdqguallichicogt <sdq.guallichico@gmail.com>
2025-02-19 12:37:57 -0500

    client-gateway: usuarios y empresas crud funcional

--- src/empresas/empresas.controller.ts
+++ src/empresas/empresas.controller.ts
@@ -9,50 +9,55 @@ import { catchError, throwError } from 'rxjs';
 export class EmpresasController {
   constructor(
     @Inject(EMPRESAS_SERVICE) private readonly empresasClient: ClientProxy
-  ) {}
+  ) { }
 
   @Post()
   async create(@Body() createEmpresaDto: CreateEmpresaDto) {
     const createdBy = createEmpresaDto.createdBy;
     //console.log('Enviando mensaje a create_empresa', createEmpresaDto);
-    return await this.empresasClient.send({cmd: 'create_empresa'}, {
+    return await this.empresasClient.send({ cmd: 'create_empresa' }, {
       createEmpresaDto,
       createdBy
     }).toPromise();
-    
+
   }
 
   @Get()
-  findAllEmpresas( ) {
-    return this.empresasClient.send({cmd: 'findAll_empresas'}, {});
+  findAllEmpresas() {
+    return this.empresasClient.send({ cmd: 'findAll_empresas' }, {});
   }
 
   @Get(':id')
   async findOne(@Param('id') emp_id: string) {
-    return this.empresasClient.send({ cmd: 'findOne_empresa'},{emp_id:Number(emp_id)})
-    .pipe(
-      catchError( err => { throw new RpcException(err)})
-    );
+    return this.empresasClient.send({ cmd: 'findOne_empresa' }, { emp_id: Number(emp_id) })
+      .pipe(
+        catchError(err => { throw new RpcException(err) })
+      );
   }
 
   @Patch(':id')
   updateEmpresa(
-    @Param('id', ParseIntPipe) emp_id: number, 
-    @Body() updateEmpresaDto: UpdateEmpresaDto) {
-    return this.empresasClient.send({cmd: 'update_empresa'},{emp_id, ... updateEmpresaDto})
-    .pipe(
-      catchError(err => { throw new RpcException( err)})
+    @Param('id', ParseIntPipe) emp_id: number,
+    @Body() updateEmpresaDto: UpdateEmpresaDto,
+    @Body('updatedBy', ParseIntPipe) updatedBy: number
+  ) {
+    return this.empresasClient.send(
+      { cmd: 'update_empresa' },
+      { updateEmpresaDto:{ ...updateEmpresaDto, emp_id }, updatedBy }
+    ).pipe(
+        catchError(err => { throw new RpcException(err) })
     );
   }
 
-  @Delete(':id')
-  removeEmpresa(@Param('id', ParseIntPipe) emp_id: number) {
-    return this.empresasClient.send({cmd: 'delete_empresa'},{emp_id})
+@Delete(':id')
+removeEmpresa(@Param('id', ParseIntPipe) emp_id: number) {
+  return this.empresasClient.send({ cmd: 'delete_empresa' }, { emp_id })
     .pipe(
       catchError(err => {
         console.log('Error al eliminar la empresa:', err);
-      return throwError(()=> new RpcException('Error al querer eliminar la empresa'))}
+        return throwError(() => new RpcException('Error al querer eliminar la empresa'))
+      }
       )
     );
-  }
+}
 }
